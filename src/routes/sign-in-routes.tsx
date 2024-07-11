ae538af34c19f9a63cd06bb269a5186b5ddbe586
Davi Silva <davisilvaphoto@gmail.com>
2024-07-11 14:48:17 -0300

    register restaurant page

--- src/routes/sign-in-routes.tsx
+++ src/routes/sign-in-routes.tsx
@@ -2,14 +2,19 @@ import { RouteObject } from 'react-router-dom'
 
 import { AuthLayout } from '@/_layouts/auth'
 import { SignIn } from '@/pages/auth/sign-in'
+import { SignUp } from '@/pages/auth/sign-up'
 
 export const signinRoutes: RouteObject = {
-  path: '/sign-in',
+  path: '/',
   element: <AuthLayout />,
   children: [
     {
       path: '/sign-in',
       element: <SignIn />,
+    },
+    {
+      path: '/sign-up',
+      element: <SignUp />,
     },
   ],
 }
