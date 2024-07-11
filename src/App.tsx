a5fb62e96fbc297c2a88106b51363468056bf350
Davi Silva <davisilvaphoto@gmail.com>
2024-07-11 14:07:57 -0300

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { routes } from './routes'
 
 export const App = () => (
   <HelmetProvider>
     <Helmet titleTemplate="%s | pizza-shop" />
+    <Toaster richColors />
     <RouterProvider router={routes} />
   </HelmetProvider>
 )
