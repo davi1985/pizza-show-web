d876fa236d7f00d7936e60fdf2d04ce4429675ea
Davi Silva <davisilvaphoto@gmail.com>
2024-07-12 11:36:51 -0300

    page not found

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,12 +1,14 @@
 import { RouteObject } from 'react-router-dom'
 
 import { AppLayout } from '@/_layouts/app'
+import { NotFound } from '@/pages/404'
 import { Dashboard } from '@/pages/app/dashboard/dashboard'
 import { Orders } from '@/pages/app/orders/orders'
 
 export const appRoutes: RouteObject = {
   path: '/',
   element: <AppLayout />,
+  errorElement: <NotFound />,
   children: [
     {
       path: '/',
