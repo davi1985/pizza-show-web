59ab4a67445c8edd4bd7422c11873b99e8c39cc2
Davi Silva <davisilvaphoto@gmail.com>
2024-07-13 16:01:58 -0300

    fix lint

--- src/pages/app/dashboard/month-revenue.tsx
+++ src/pages/app/dashboard/month-revenue.tsx
@@ -3,27 +3,25 @@ import { DollarSign } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { currencyFormatter } from '@/utils/currencyFormatter'
 
-export const MonthRevenue = () => {
-  return (
-    <Card>
-      <CardHeader className="flex-row items-center justify-between space-x-0 pb-2">
-        <CardTitle className="text-base font-semibold">
-          Receita total (mês)
-        </CardTitle>
-        <DollarSign className="h4 w-4 text-muted-foreground" />
-      </CardHeader>
+export const MonthRevenue = () => (
+  <Card>
+    <CardHeader className="flex-row items-center justify-between space-x-0 pb-2">
+      <CardTitle className="text-base font-semibold">
+        Receita total (mês)
+      </CardTitle>
+      <DollarSign className="h4 w-4 text-muted-foreground" />
+    </CardHeader>
 
-      <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">
-          {currencyFormatter(1230.8)}
-        </span>
-        <p className="text-xs text-muted-foreground">
-          <span className="data:text-emerald-400 font-medium text-emerald-500">
-            +2%
-          </span>{' '}
-          em relação ao mês passado.
-        </p>
-      </CardContent>
-    </Card>
-  )
-}
+    <CardContent className="space-y-1">
+      <span className="text-2xl font-bold tracking-tight">
+        {currencyFormatter(1230.8)}
+      </span>
+      <p className="text-xs text-muted-foreground">
+        <span className="data:text-emerald-400 font-medium text-emerald-500">
+          +2%
+        </span>{' '}
+        em relação ao mês passado.
+      </p>
+    </CardContent>
+  </Card>
+)
--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -14,96 +14,90 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
-export const OrderDetails = () => {
-  return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Pedido: 12233ddsee</DialogTitle>
-        <DialogDescription>Detalhes do pedido</DialogDescription>
-      </DialogHeader>
+export const OrderDetails = () => (
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Pedido: 12233ddsee</DialogTitle>
+      <DialogDescription>Detalhes do pedido</DialogDescription>
+    </DialogHeader>
 
-      <div className="space-y-6">
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell className="text-muted-foreground">Status</TableCell>
+    <div className="space-y-6">
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="text-muted-foreground">Status</TableCell>
 
-              <TableCell className="flex justify-end">
-                <div className="flex items-center gap-2">
-                  <span className="h-2 w-2 rounded-full bg-slate-400" />
-                  <span className="font-medium text-muted-foreground">
-                    Pendente
-                  </span>
-                </div>
-              </TableCell>
-            </TableRow>
+            <TableCell className="flex justify-end">
+              <div className="flex items-center gap-2">
+                <span className="h-2 w-2 rounded-full bg-slate-400" />
+                <span className="font-medium text-muted-foreground">
+                  Pendente
+                </span>
+              </div>
+            </TableCell>
+          </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">Client</TableCell>
+          <TableRow>
+            <TableCell className="text-muted-foreground">Client</TableCell>
 
-              <TableCell className="flex justify-end">John Doe</TableCell>
-            </TableRow>
+            <TableCell className="flex justify-end">John Doe</TableCell>
+          </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">Telefone</TableCell>
+          <TableRow>
+            <TableCell className="text-muted-foreground">Telefone</TableCell>
 
-              <TableCell className="flex justify-end">
-                (88) 9 8877 6644
-              </TableCell>
-            </TableRow>
+            <TableCell className="flex justify-end">(88) 9 8877 6644</TableCell>
+          </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">E-mail</TableCell>
+          <TableRow>
+            <TableCell className="text-muted-foreground">E-mail</TableCell>
 
-              <TableCell className="flex justify-end">jonh@email.com</TableCell>
-            </TableRow>
+            <TableCell className="flex justify-end">jonh@email.com</TableCell>
+          </TableRow>
 
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Realizado há
-              </TableCell>
+          <TableRow>
+            <TableCell className="text-muted-foreground">
+              Realizado há
+            </TableCell>
 
-              <TableCell className="flex justify-end">há 3 minutos</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+            <TableCell className="flex justify-end">há 3 minutos</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
 
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Produto</TableHead>
-              <TableHead className="text-right">Qtd.</TableHead>
-              <TableHead className="text-right">Preço</TableHead>
-              <TableHead className="text-right">Subtotal</TableHead>
-            </TableRow>
-          </TableHeader>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Produto</TableHead>
+            <TableHead className="text-right">Qtd.</TableHead>
+            <TableHead className="text-right">Preço</TableHead>
+            <TableHead className="text-right">Subtotal</TableHead>
+          </TableRow>
+        </TableHeader>
 
-          <TableBody>
-            <TableRow>
-              <TableCell>Pizza Peperoni Família</TableCell>
-              <TableCell className="text-right">2</TableCell>
-              <TableCell className="text-right">R$ 50,00</TableCell>
-              <TableCell className="text-right">R$ 100,00</TableCell>
-            </TableRow>
+        <TableBody>
+          <TableRow>
+            <TableCell>Pizza Peperoni Família</TableCell>
+            <TableCell className="text-right">2</TableCell>
+            <TableCell className="text-right">R$ 50,00</TableCell>
+            <TableCell className="text-right">R$ 100,00</TableCell>
+          </TableRow>
 
-            <TableRow>
-              <TableCell>Pizza Mussarela</TableCell>
-              <TableCell className="text-right">2</TableCell>
-              <TableCell className="text-right">R$ 30,00</TableCell>
-              <TableCell className="text-right">R$ 60,00</TableCell>
-            </TableRow>
-          </TableBody>
+          <TableRow>
+            <TableCell>Pizza Mussarela</TableCell>
+            <TableCell className="text-right">2</TableCell>
+            <TableCell className="text-right">R$ 30,00</TableCell>
+            <TableCell className="text-right">R$ 60,00</TableCell>
+          </TableRow>
+        </TableBody>
 
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total do pedido</TableCell>
-              <TableCell className="text-right font-medium">
-                R$ 160,00
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
-    </DialogContent>
-  )
-}
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total do pedido</TableCell>
+            <TableCell className="text-right font-medium">R$ 160,00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  </DialogContent>
+)
