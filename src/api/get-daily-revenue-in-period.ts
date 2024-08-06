a383df5f6a3b8ac621fe535aee67553e77af9eea
Davi Silva <davisilvaphoto@gmail.com>
2024-08-06 08:50:06 -0300

    revenue period chart

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,27 @@
 import { api } from '@/lib/axios'
 
+type GetDailyRevenueInPeriodRequest = {
+  from?: Date
+  to?: Date
+}
+
 type GetDailyRevenueInPeriodResponse = {
   date: string
   receipt: number
 }[]
 
-export const getDailyRevenueInPeriod = async () => {
+export const getDailyRevenueInPeriod = async ({
+  from,
+  to,
+}: GetDailyRevenueInPeriodRequest) => {
   const { data } = await api.get<GetDailyRevenueInPeriodResponse>(
     '/metrics/daily-receipt-in-period',
+    {
+      params: {
+        from,
+        to,
+      },
+    },
   )
 
   return data
